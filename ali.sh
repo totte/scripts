1bdf74fad40ed33875371ac69e105a169ab83fb8
totte <totte@tott.es>
2012-10-07 15:35:39 +0200

    Fix LOCALE setting in ali.sh

--- ali.sh
+++ ali.sh
@@ -146,9 +146,12 @@ set_keymap()
 set_locale()
 {
     echo `date "+%H:%M:%S"` "Setting locale..."
-    echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+    echo "LANG=en_GB.UTF-8" > /mnt/etc/locale.conf
     echo "LC_COLLATE=C" >> /mnt/etc/locale.conf
+    echo "LC_TIME=en_GB.UTF-8" >> /mnt/etc/locale.conf
+    echo "en_GB.UTF-8 UTF-8" >> /mnt/etc/locale.gen
     echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+    echo "sv_SE.UTF-8 UTF-8" >> /mnt/etc/locale.gen
     chroot /mnt /usr/sbin/locale-gen
 }
 
