210bbdec9d8e63609fbdcb348f0dbabb4635a7fb
totte <totte@tott.es>
2012-10-08 18:48:30 +0200

    Minor tweaks

--- ali.sh
+++ ali.sh
@@ -88,7 +88,7 @@ install_packages()
     # Keep trying until success
     result=1
     until [ $result -eq 0 ]; do
-        pacman --root /mnt --cachedir /mnt/var/cache/pacman/pkg -Sy abs alsa-utils base base-devel git hsetroot kde{multimedia-juk,pim-{akonadiconsole,akregator,console,kaddressbook,kalarm,kmail,knode,kontact,korganizer,ktimetracker},pimlibs} lsb-release mesa mysql openssh opera phonon pkgtools pyqt python python-pip qt qtfm rxvt-unicode slim slock sshfs sudo syslinux systemd systemd-arch-units terminus-font tmux ttf-{droid,inconsolata} unclutter vim wget wicd xmobar xmonad xmonad-contrib xorg-{server,server-utils,utils,xinit} zsh xf86-video-nouveau
+        pacman --root /mnt --cachedir /mnt/var/cache/pacman/pkg -Sy abs alsa-utils base base-devel git hsetroot kde{multimedia-juk,pim-{akonadiconsole,akregator,console,kaddressbook,kalarm,kmail,knode,kontact,korganizer,ktimetracker},pimlibs,utils-{kcalc,kgpg,kwallet}} lsb-release mesa mysql openssh opera phonon pkgtools pyqt python python-pip qt qtfm rxvt-unicode slim slock sshfs sudo syslinux systemd systemd-arch-units terminus-font tmux ttf-{droid,inconsolata} unclutter vim wget wicd xmobar xmonad xmonad-contrib xorg-{server,server-utils,utils,xinit} zsh xf86-video-nouveau
         result=$?
     done
 }
